3a0c7caadb46a69fb29fe34bd64de28c9b263fd6
Jochem Kossen <jochem.kossen@gmail.com>
2010-11-09 22:52:07 +0100

    Add rest_to_html_fragment to be able to convert just the body part

--- restconverter.py
+++ restconverter.py
@@ -9,6 +9,9 @@
     This class heavily depends on the functionality provided by the docutils
     package.
 
+
+    See http://wiki.python.org/moin/ReStructuredText for more information
+
     :copyright: (c) 2010 by Jochem Kossen.
     :license: BSD, see LICENSE for more details.
 """
@@ -32,4 +35,10 @@ html_fragment_writer.translator_class = HTMLFragmentTranslator
 def rest_to_html(s):
     """Convert ReST input to HTML output"""
     return core.publish_string(s, writer=html_fragment_writer)
+
+def rest_to_html_fragment(s):
+    parts = core.publish_parts(
+                          source=s,
+                          writer_name='html')
+    return parts['body_pre_docinfo']+parts['fragment']
 
